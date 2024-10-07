aa6502c593915d1ac4ac23f1f0609814286e7903
manuel <manuelfqc@gmail.com>
2024-10-06 20:01:00 -0500

    Done backend frontend

--- backend/Dto/LoginDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace backend.Dto
-{
-    public class LoginDto
-    {
-        public string Email { get; set; } = String.Empty;
-        public string Password { get; set; } = String.Empty;
-        public string Username { get; set; } = String.Empty;
-    }
-}
